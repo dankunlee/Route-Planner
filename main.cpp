b504549ee46ad3107c101229f3e7cc2e06837262
Kun Lee <45495755+dankunlee@users.noreply.github.com>
2019-05-18 08:57:32 -0400

    Add comments

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::cout;
 using std::ifstream;
@@ -19,7 +20,9 @@ using std::string;
 using std::vector;
 
 enum class State {kEmpty, kObstacle};
+const int direction[4][2] {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
+//convert a string line from board to accessible states
 vector<State> parseLine(string line) {
     int n;
     char s;
@@ -32,6 +35,7 @@ vector<State> parseLine(string line) {
     return row;
 }
 
+//read the board from given file
 vector<vector<State>> readBoardFile(string fileName) {
     ifstream file(fileName);
     vector<vector<State> > board;
@@ -44,15 +48,22 @@ vector<vector<State>> readBoardFile(string fileName) {
     return board;
 }
 
+//convert board states to user friendly items
 string cellString(State state) {
     switch (state) {
-        case State::kEmpty:
-            return "0   ";
         case State::kObstacle:
-            return "⛰️   ";
+            return "Δ   ";
+        case State::kPath:
+            return "*   ";
+        case State::kStart:
+            return "S   ";
+        case State::kStop:
+            return "S   ";
+        default: return "0   ";
     }
 }
 
+//print the board
 void printBoard(const vector<vector<State>> board) {
     for (vector<State> i: board) {
         for (State j: i) {
